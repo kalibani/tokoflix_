44091b25b327e98ca451ad6cc86b14a6f6a3cdfd
Kautzar Alibani <kautzar.alibani@modalrakyat.id>
2019-01-31 15:07:24 +0700

    Initial commit

---
+++ build/precache-manifest.c89768b1c3171fccb8335e076968fcd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e446ad37e721f20129b",
+    "url": "/static/js/main.0e446ad3.chunk.js"
+  },
+  {
+    "revision": "92bdfe4802a3d24a1a44",
+    "url": "/static/js/1.92bdfe48.chunk.js"
+  },
+  {
+    "revision": "0e446ad37e721f20129b",
+    "url": "/static/css/main.f2814a2c.chunk.css"
+  },
+  {
+    "revision": "92bdfe4802a3d24a1a44",
+    "url": "/static/css/1.9bcb2e30.chunk.css"
+  },
+  {
+    "revision": "0cf530b16656ad79e8e17f9ef84e2f7e",
+    "url": "/index.html"
+  }
+];
